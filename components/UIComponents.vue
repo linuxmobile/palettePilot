3bc70928d625acab3fd10497c0a50cde9eadb9d2
Braian A. Diez <bdiez19@gmail.com>
2024-02-16 14:08:54 -0300

    feat: added github repo example component

--- components/UIComponents.vue
+++ components/UIComponents.vue
@@ -1,10 +1,14 @@
 <script setup>
 import MemberCard from '~/components/uicomponents/MemberCard.vue'
+import GithubRepo from '~/components/uicomponents/GithubRepo.vue'
 </script>
 <template>
-  <main class="md:col-span-9 pt-18 min-h-screen w-full px-3 grid grid-cols-9">
+  <main class="md:col-span-9 pt-18 min-h-screen w-full px-3 grid grid-cols-9 gap-x-4 gap-y-3">
     <section class="md:col-span-3">
       <MemberCard />
+    </section>
+    <section class="md:col-span-3">
+      <GithubRepo />
     </section>
   </main>
 </template>
---
+++ components/uicomponents/GithubRepo.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useColorSelection } from '~/composables/useColorSelection.ts'
+
+const {accentColor, primaryColor} = useColorSelection()
+
+const bg = computed(() => primaryColor.value ? primaryColor.value.hex : '#FFFFFF'); 
+const fg = computed(() => accentColor.value ? accentColor.value.hex : '#000000'); 
+
+const repoData = useState('repoData', () => ({
+  title: 'palettePilot',
+  description: 'Explore Palette Pilot magic: transforming your images into color palettes and giving UI ideas. Perfect for creatives',
+  language: 'Vue',
+  stars: '1.2k',
+  updated: 'Updated January 2024'
+}))
+
+
+</script>
+<template>
+  <article class="border border-gray-600 rounded-xl px-8 py-5 flex flex-col gap-y-2" :style="{backgroundColor: bg, color: fg}">
+    <section class="flex flex-col">
+      <div class="flex justify-between w-full items-center">
+        <h2 class="font-bold">{{repoData.title}}</h2>
+        <div class="rounded-lg px-3 py-1 border" :style="{borderColor:fg}">★ Star</div>
+      </div>
+      <p class="opacity-70">{{repoData.description}}</p>
+    </section>
+    <footer class="flex gap-x-5 opacity-80">
+      <p><span class="text-[#41B883]">●</span> {{repoData.language}}</p>
+      <p>★ {{repoData.stars}}</p>
+      <p>{{repoData.updated}}</p>
+    </footer>
+  </article>
+</template>
