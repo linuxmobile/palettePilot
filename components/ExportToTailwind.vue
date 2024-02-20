6aa2f650f5af0b8e04187be856d2fa8a60824bc2
Braian A. Diez <bdiez19@gmail.com>
2024-02-20 20:44:53 -0300

    feat: added combined palette btw primary & accent

--- components/ExportToTailwind.vue
+++ components/ExportToTailwind.vue
@@ -34,8 +34,45 @@ function generateColorScale(baseColorHex, prefix) {
   }
 }
 
+const lighterColor = computed(() => {
+  const primary = primaryColor.value ? chroma(primaryColor.value.hex) : chroma('#FFFFFF')
+  const accent = accentColor.value ? chroma(accentColor.value.hex) : chroma('#000000')
+  return primary.luminance() > accent.luminance() ? accent.hex() : primary.hex()
+})
+
+const darkerColor = computed(() => {
+  const primary = primaryColor.value ? chroma(primaryColor.value.hex) : chroma('#FFFFFF')
+  const accent = accentColor.value ? chroma(accentColor.value.hex) : chroma('#000000')
+  return primary.luminance() <= accent.luminance() ? accent.hex() : primary.hex()
+})
+
+function generateIntegratedColorScale() {
+  const primary = primaryColor.value ? chroma(primaryColor.value.hex) : chroma('#FFFFFF');
+  const accent = accentColor.value ? chroma(accentColor.value.hex) : chroma('#000000');
+
+  const startColor = primary.luminance() > accent.luminance() ? primary : accent;
+  const endColor = primary.luminance() <= accent.luminance() ? primary : accent;
+
+  let scale = chroma.scale([startColor, endColor])
+    .mode('lab')
+    .colors(11);
+
+  const colorObject = scale.reduce((acc, color, index) => {
+    const baseLevels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+    acc[baseLevels[index]] = color;
+    return acc;
+  }, {});
+
+  return {
+    integrated: colorObject
+  };
+}
+
+
+const integratedPalette = computed(() => generateIntegratedColorScale())
 const primaryPalette = computed(() => generateColorScale(bg.value, 'primary'))
 const accentPalette = computed(() => generateColorScale(fg.value, 'accent'))
+
 const visible = useState('modalVisible', () => false)
 
 function paletteToString(palette) {
@@ -55,6 +92,7 @@ function paletteToString(palette) {
 
 const primaryPaletteString = computed(() => paletteToString(primaryPalette))
 const accentPaletteString = computed(() => paletteToString(accentPalette))
+const integratedPaletteString = computed(() => paletteToString(integratedPalette))
 
 const copyToClipboard = async text => {
   try {
@@ -97,12 +135,14 @@ const copyToClipboard = async text => {
           <h3>Export Code</h3>
         </header>
         <div
-          class="w-full grid grid-cols-2 place-content-center gap-x-24 gap-y-3 [&>h4]:font-semibold"
+          class="w-full grid grid-cols-3 place-content-center gap-x-24 gap-y-3 [&>h4]:font-semibold"
         >
           <h4 class="text-white text-lg">Primary Palette</h4>
           <h4 class="text-white text-lg">Accent Palette</h4>
+          <h4 class="text-white text-lg">From Primary to Accent</h4>
           <pre>{{ primaryPaletteString }},</pre>
           <pre>{{ accentPaletteString }},</pre>
+          <pre>{{ integratedPaletteString }}</pre>
         </div>
         <footer class="flex flex-col gap-4 w-full">
           <div class="flex flex-col gap-2">
@@ -112,7 +152,7 @@ const copyToClipboard = async text => {
                 v-for="key in Object.keys(primaryPalette.primary)"
                 :key="primaryPalette.primary[key]"
                 v-tooltip.top="`${key}: ${primaryPalette.primary[key]}`"
-                class="w-[32px] h-[32px]"
+                class="aspect-square w-full h-auto"
                 :style="{ backgroundColor: primaryPalette.primary[key] }"
                 @click="() => copyToClipboard(primaryPalette.primary[key])"
               >
@@ -129,12 +169,29 @@ const copyToClipboard = async text => {
                 v-for="key in Object.keys(accentPalette.accent)"
                 :key="accentPalette.accent[key]"
                 v-tooltip.top="`${key}: ${accentPalette.accent[key]}`"
-                class="w-[32px] h-[32px]"
+                class="aspect-square w-full h-auto"
                 :style="{ backgroundColor: accentPalette.accent[key] }"
                 @click="() => copyToClipboard(accentPalette.accent[key])"
               >
                 <span class="sr-only">
                   Copy hex color {{ accentPalette.accent[key] }} to clipboard
+                </span>
+              </button>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <h5 class="font-bold text-white text-sm">From Primary to Accent</h5>
+            <div class="flex">
+              <button
+                v-for="key in Object.keys(integratedPalette.integrated)"
+                :key="integratedPalette.integrated[key]"
+                v-tooltip.top="`${key}: ${integratedPalette.integrated[key]}`"
+                class="aspect-square w-full h-auto"
+                :style="{ backgroundColor: integratedPalette.integrated[key] }"
+                @click="() => copyToClipboard(integratedPalette.integrated[key])"
+              >
+                <span class="sr-only">
+                  Copy hex color {{ integratedPalette.integrated[key] }} to clipboard
                 </span>
               </button>
             </div>
