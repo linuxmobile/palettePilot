1a733d362cb1823a4f0464bfc290da3c06b441fd
Braian A. Diez <bdiez19@gmail.com>
2024-02-20 21:05:14 -0300

    fix: fixing styles responsive

--- components/ExportToTailwind.vue
+++ components/ExportToTailwind.vue
@@ -127,7 +127,7 @@ const copyToClipboard = async text => {
   >
     <template #container="{ closeCallback }">
       <section
-        class="flex flex-col items-center justify-center gap-3 px-12 py-14"
+        class="flex flex-col items-center justify-center gap-3 px-12 py-6 2xl:py-14"
       >
         <header
           class="w-full border-b border-gray-500 font-semibold text-xl text-white"
@@ -135,24 +135,24 @@ const copyToClipboard = async text => {
           <h3>Export Code</h3>
         </header>
         <div
-          class="w-full grid grid-cols-3 place-content-center gap-x-24 gap-y-3 [&>h4]:font-semibold"
+          class="w-full grid grid-cols-3 place-content-center gap-x-24 gap-y-3 [&>h4]:font-semibold [&>h4]:text-sm [&>h4]:2xl:text-lg [&>h4]:text-white [&>pre]:text-sm [&>pre]:2xl:text-normal"
         >
-          <h4 class="text-white text-lg">Primary Palette</h4>
-          <h4 class="text-white text-lg">Accent Palette</h4>
-          <h4 class="text-white text-lg">From Primary to Accent</h4>
+          <h4>Primary Palette</h4>
+          <h4>Accent Palette</h4>
+          <h4>From Primary to Accent</h4>
           <pre>{{ primaryPaletteString }},</pre>
           <pre>{{ accentPaletteString }},</pre>
           <pre>{{ integratedPaletteString }}</pre>
         </div>
         <footer class="flex flex-col gap-4 w-full">
-          <div class="flex flex-col gap-2">
+          <div class="flex flex-col gap-1">
             <h5 class="font-bold text-white text-sm">Primary</h5>
             <div class="flex">
               <button
                 v-for="key in Object.keys(primaryPalette.primary)"
                 :key="primaryPalette.primary[key]"
                 v-tooltip.top="`${key}: ${primaryPalette.primary[key]}`"
-                class="aspect-square w-full h-auto"
+                class="aspect-square w-full md:h-10 2xl:h-auto"
                 :style="{ backgroundColor: primaryPalette.primary[key] }"
                 @click="() => copyToClipboard(primaryPalette.primary[key])"
               >
@@ -162,14 +162,14 @@ const copyToClipboard = async text => {
               </button>
             </div>
           </div>
-          <div class="flex flex-col gap-2">
+          <div class="flex flex-col gap-1">
             <h5 class="font-bold text-white text-sm">Accent</h5>
             <div class="flex">
               <button
                 v-for="key in Object.keys(accentPalette.accent)"
                 :key="accentPalette.accent[key]"
                 v-tooltip.top="`${key}: ${accentPalette.accent[key]}`"
-                class="aspect-square w-full h-auto"
+                class="aspect-square w-full md:h-10 2xl:h-auto"
                 :style="{ backgroundColor: accentPalette.accent[key] }"
                 @click="() => copyToClipboard(accentPalette.accent[key])"
               >
@@ -179,14 +179,14 @@ const copyToClipboard = async text => {
               </button>
             </div>
           </div>
-          <div class="flex flex-col gap-2">
+          <div class="flex flex-col gap-1">
             <h5 class="font-bold text-white text-sm">From Primary to Accent</h5>
             <div class="flex">
               <button
                 v-for="key in Object.keys(integratedPalette.integrated)"
                 :key="integratedPalette.integrated[key]"
                 v-tooltip.top="`${key}: ${integratedPalette.integrated[key]}`"
-                class="aspect-square w-full h-auto"
+                class="aspect-square w-full md:h-10 2xl:h-auto"
                 :style="{ backgroundColor: integratedPalette.integrated[key] }"
                 @click="() => copyToClipboard(integratedPalette.integrated[key])"
               >
@@ -198,7 +198,7 @@ const copyToClipboard = async text => {
           </div>
           <button
             @click="closeCallback"
-            class="mt-4 p-3 w-full text-primary-50 border-1 border-white/30 rounded-md bg-gray-200 hover:bg-gray-300 dark:bg-neutral-800 dark:border-neutral-700 hover:dark:bg-neutral-700"
+            class="mt-4 p-1 2xl:p-3 w-full text-primary-50 border-1 border-white/30 rounded-md bg-gray-200 hover:bg-gray-300 dark:bg-neutral-800 dark:border-neutral-700 hover:dark:bg-neutral-700"
           >
             Cancel
           </button>
