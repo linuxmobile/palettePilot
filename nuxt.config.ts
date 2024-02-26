b2cf540a9ba776de1840cb59b4a1efb359b43ac4
Ivan Atias <ivan.d.atias@gmail.com>
2024-02-25 21:14:24 -0400

    👀 chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,48 @@
 export default defineNuxtConfig({
-	site: {
-		url: "https://palette-pilot.vercel.app/",
-		name: "Palette Pilot - Generate Awesome Color Palette for you UI",
-		description:
-			"Explore Palette Pilot magic: transforming your images into color palettes and giving UI ideas. Perfect for creatives",
-		defaultLocale: "en",
-	},
-	app: {
-		head: {
-			charset: "utf-8",
-			viewport: "width=device-width, initial-scale=1",
-		},
-	},
-	modules: [
-		"@unocss/nuxt",
-		"nuxt-primevue",
-		"@nuxtjs/color-mode",
-		"@nuxtjs/seo",
-	],
-	unocss: {
-		attributify: true,
-	},
-	css: ["@unocss/reset/tailwind.css"],
-	colorMode: {
-		preference: "system",
-		fallback: "dark",
-		classSuffix: "",
-	},
-	devtools: { enabled: true },
-	primevue: {
-		directives: {
-			include: ["Tooltip"],
-		},
-	},
-});
+  site: {
+    url: 'https://palette-pilot.vercel.app/',
+    name: 'Palette Pilot - Generate Awesome Color Palette for you UI',
+    description:
+      'Explore Palette Pilot magic: transforming your images into color palettes and giving UI ideas. Perfect for creatives',
+    defaultLocale: 'en'
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+  },
+  nitro: {
+    storage: {
+      data: { driver: 'vercelKV' }
+    }
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: import.meta.env.PROD
+        ? 'https://palette-pilot.vercel.app'
+        : 'http://localhost:3000'
+    }
+  },
+  modules: [
+    '@unocss/nuxt',
+    'nuxt-primevue',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/seo'
+  ],
+  unocss: {
+    attributify: true
+  },
+  css: ['@unocss/reset/tailwind.css'],
+  colorMode: {
+    preference: 'system',
+    fallback: 'dark',
+    classSuffix: ''
+  },
+  devtools: { enabled: true },
+  primevue: {
+    directives: {
+      include: ['Tooltip']
+    }
+  }
+})
