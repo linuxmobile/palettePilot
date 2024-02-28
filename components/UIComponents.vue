9ecc9955a7e22cbc4b58a62f2562d58e799bd1c3
monoald <carlos.aldazosa@outlook.com>
2024-02-28 18:23:40 -0400

    [FIX] UI Components grid columns according to screen size

--- components/UIComponents.vue
+++ components/UIComponents.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="pt-12 w-full h-full px-3 pb-6 grid content-start grid-cols-8 gap-x-4 gap-y-3 select-none z-[1]"
+    class="pt-12 w-full h-full px-3 pb-6 grid content-start grid-cols-8 2xl:grid-cols-9 gap-x-4 gap-y-3 select-none z-[1]"
   >
     <section class="col-span-full md:col-span-4 2xl:col-span-3 flex flex-col gap-y-3">
       <uicomponentsMemberCard />
